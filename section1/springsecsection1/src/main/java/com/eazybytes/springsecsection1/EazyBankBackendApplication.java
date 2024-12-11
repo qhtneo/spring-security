1c13d8b612ed4cb0fe48ab06eca7e96fefcb321d
qhtneo <hj9671@gmail.com>
2024-12-11 17:52:33 +0900

    comment(EazyBankBackendApplication.java) : @ComponentScan 의미

--- section1/springsecsection1/src/main/java/com/eazybytes/springsecsection1/EazyBankBackendApplication.java
+++ section1/springsecsection1/src/main/java/com/eazybytes/springsecsection1/EazyBankBackendApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-// @ComponentScan("com.eazybytes.springsecsection1.controller")
+// @ComponentScan("com.eazybytes.springsecsection1.controller") // 컨트롤러를 메인 패키지 밖에 선언한 경우엔 컴포넌트 스캔 사용
 public class EazyBankBackendApplication {
 
 	public static void main(String[] args) {
@@ -13,3 +13,4 @@ public class EazyBankBackendApplication {
 	}
 
 }
+
